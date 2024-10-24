438b1a2fd54a14653eb63ba11d9a148c2d47b011
JuliBosch <julibosch99@gmail.com>
2024-10-24 19:42:38 -0300

    reglas validacion para inmueble

---
+++ src/libs/inmuebleValidation.ts
@@ -0,0 +1,15 @@
+import { body, param } from "express-validator";
+
+// Definir las validaciones para `Inmueble`
+const reglasValidacionInmueble = [
+  body("calle").isString().withMessage("La calle debe ser un string").notEmpty().withMessage("La calle no puede estar vacía"),
+  body("altura").optional().isString().withMessage("La altura debe ser un string"),
+  body("localidad").isString().withMessage("La localidad debe ser un string").notEmpty().withMessage("La localidad no puede estar vacía"),
+  body("piso").optional().isString().withMessage("El piso debe ser un string"),
+  body("departamento").optional().isString().withMessage("El departamento debe ser un string"),
+  body("locadorId").isInt().withMessage("El locadorId debe ser un entero").notEmpty().withMessage("El locadorId no puede estar vacío"),
+];
+
+const reglaParamInmueble = param('id').isInt().withMessage("ID no válido.");
+
+export { reglasValidacionInmueble, reglaParamInmueble };
--- src/router/inmuebleRoutes.ts
+++ src/router/inmuebleRoutes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { handleInputsErrors } from "../middleware/locatarioMid";
 import { listadoInmuebles, obtenerInmueble, crearInmueble, editarInmueble, eliminarInmueble } from "../controller/inmuebleController";
-// Faltan reglas de validacion
+import { reglaParamInmueble, reglasValidacionInmueble } from "../libs/inmuebleValidation";
 
 const router = Router();
 
 router.get("/inmuebles", handleInputsErrors as any, listadoInmuebles as any)
-router.get("/inmuebles/:id", handleInputsErrors as any, obtenerInmueble as any)
-router.post("/inmuebles", handleInputsErrors as any, crearInmueble as any)
-router.put("/inmuebles/:id", handleInputsErrors as any, editarInmueble as any)
-router.delete("/inmuebles/:id", handleInputsErrors as any, eliminarInmueble as any)
+router.get("/inmuebles/:id", reglaParamInmueble, handleInputsErrors as any, obtenerInmueble as any)
+router.post("/inmuebles", reglasValidacionInmueble, handleInputsErrors as any, crearInmueble as any)
+router.put("/inmuebles/:id", reglaParamInmueble, reglasValidacionInmueble, handleInputsErrors as any, editarInmueble as any)
+router.delete("/inmuebles/:id", reglaParamInmueble, handleInputsErrors as any, eliminarInmueble as any)
 
 export default router;
